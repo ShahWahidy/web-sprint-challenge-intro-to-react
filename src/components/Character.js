5fe667bb3d5c3d2f34a12a254b0e4c559040443f
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2021-12-10 08:44:17 -0800

    populated all names to the dom

--- src/components/Character.js
+++ src/components/Character.js
@@ -7,13 +7,18 @@ import styled from "styled-components";
 const CharacterCard = (props) => {
     console.log(props);
     const {data} = props;
-    //console.log(data[0].name);
+   
     const newData = data.map(person => person.name)
     console.log(newData)
     return(
         <div className="contents" >
             <div className="personDetails">
                   <div className="luke"> {newData[0]} </div>
+                  <div className="cThree"> {newData[1]} </div>
+                  <div className="rTwo"> {newData[2]} </div>
+                  <div className="darthVader"> {newData[3]} </div>
+                  <div className="leia"> {newData[4]} </div>
+                  <div className="owen"> {newData[5]} </div>
             </div>       
         </div>
     )
