945258eecbe925e19b00a36381898a48be23e603
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2021-12-10 09:41:57 -0800

    last pus -> styled the components

--- src/App.js
+++ src/App.js
@@ -10,13 +10,17 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [baseURL, setBaseURL] = useState('https://swapi.dev/api/people')
-  const [character, setCharacter] = useState([]);
 
+  //using state to save URL incase given API is down
+  const [baseURL, setBaseURL] = useState('https://swapi.dev/api/people')
+  //using useState to store fetch the data from the API
+  const [character, setCharacter] = useState([]);
+  //useEffect contains the axios call and keeps it from rendering multiple times
   useEffect(() => {
     axios.get(baseURL)
     .then((res) => {
       //console.log(res.data);
+      //setCharacter function sends the data to character to line 17
       setCharacter(res.data);
     })
     .catch((err) => {
@@ -24,9 +28,11 @@ const App = () => {
     })
   }, [])
 
+  //return statment 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {/* charcterCard is imported on line 4 and data props is passed to it */}
       <CharacterCard data={character} />
     </div>
   );
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,24 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 
+//styling the div
+const StyledDiv = styled.div`
 
+  width: 94%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+  color: white;
+  border: 3px solid;
+  font-size: xx-large;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: royalblue;
+  }
+
+
+`
 
 
 const CharacterCard = (props) => {
-    console.log(props);
+    //console.log(props);
+
+    //destructuring props 
     const {data} = props;
-   
+   // saving the names into a new array
     const newData = data.map(person => person.name)
-    console.log(newData)
+    //console.log(newData)
     return(
         <div className="contents" >
+            {/* content class contains all the names from the array and are being pushed to DOM */}
             <div className="personDetails">
-                  <div className="luke"> {newData[0]} </div>
-                  <div className="cThree"> {newData[1]} </div>
-                  <div className="rTwo"> {newData[2]} </div>
-                  <div className="darthVader"> {newData[3]} </div>
-                  <div className="leia"> {newData[4]} </div>
-                  <div className="owen"> {newData[5]} </div>
+                  <StyledDiv className="luke"> {newData[0]} </StyledDiv>
+                  <StyledDiv  className="cThree"> {newData[1]} </StyledDiv >
+                  <StyledDiv  className="rTwo"> {newData[2]} </StyledDiv >
+                  <StyledDiv  className="darthVader"> {newData[3]} </StyledDiv >
+                  <StyledDiv className="leia"> {newData[4]} </StyledDiv >
+                  <StyledDiv  className="owen"> {newData[5]} </StyledDiv >
             </div>       
         </div>
     )
@@ -28,4 +49,5 @@ const CharacterCard = (props) => {
 
 export default CharacterCard;
 
+//line of code that worked but didnt pass the codegrade
 //{data.map(person =><div className="person">{person.name}</div>)}  
