32204b0db8f0a7b9849a8e0d490a421c8cc9d1ca
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2021-12-10 08:32:41 -0800

    third push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect}  from 'react';
 import './App.css';
+import axios from 'axios';
+import CharacterCard from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,24 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [baseURL, setBaseURL] = useState('https://swapi.dev/api/people')
+  const [character, setCharacter] = useState([]);
+
+  useEffect(() => {
+    axios.get(baseURL)
+    .then((res) => {
+      //console.log(res.data);
+      setCharacter(res.data);
+    })
+    .catch((err) => {
+      console.error('something ain\'t working')
+    })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <CharacterCard data={character} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+
+
+
+const CharacterCard = (props) => {
+    console.log(props);
+    const {data} = props;
+    //console.log(data[0].name);
+    const newData = data.map(person => person.name)
+    console.log(newData)
+    return(
+        <div className="contents" >
+            <div className="personDetails">
+                  {newData[0]}
+            </div>       
+        </div>
+    )
+}
+
+
+
+export default CharacterCard;
+
+//{data.map(person =><div className="person">{person.name}</div>)}  
